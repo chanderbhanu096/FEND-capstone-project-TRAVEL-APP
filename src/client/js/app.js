2b6c8906437c61e3c6527e2f75c58d49828c5257
Chander Bhanu <chanderbhanu096@gmail.com>
2020-07-22 14:25:45 +0530

    commit

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -2,6 +2,7 @@ import{geo_Api,weatherAPI,PixaBayAPI} from './datafetchfromapis'
 
 //post Data to the server
 
+var submitcounter = 0 ;
 const postDataToServer = async (Url = '', data = {}) => {
     //body of the response
     const response = await fetch(Url, {
@@ -30,7 +31,7 @@ const postDataToServer = async (Url = '', data = {}) => {
     const place_name = document.getElementById('placetogo').value;
 
     // fetch date from the userinput
-    const date1 = document.getElementById('dateid').value;
+    const date = document.getElementById('dateid').value;
     
     //current date of the system
     // const getCurrentDate = (addDate = 0) => {
@@ -44,13 +45,18 @@ const postDataToServer = async (Url = '', data = {}) => {
     //     return `${newDate.getFullYear()}-${month}-${date}`;
     // };
 
-    const date = new Date(); 
-    console.log(date);
-    console.log(date1);
+    const date1 = new Date(); 
+    const date2 = new Date(date);
 
-    var date2 = date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate();
-    //TO FIND THE DIFFERENCE BETWEEN THE CURRENT TIME AND THE USER INPUTTED TIME
-    var Difference_In_Days = date1 - date2;
+   
+    var Difference_In_Time = date2.getTime() - date1.getTime();
+
+    // To calculate the no. of days between two dates 
+    var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+   
+    console.log(Difference_In_Days);
+    
+    
 
 
     if(place_name=='')
@@ -58,21 +64,16 @@ const postDataToServer = async (Url = '', data = {}) => {
         document.getElementById('placetogo').style.cssText="border:1px solid red";
         document.getElementById('nameerror').innerHTML="enter name";
     }
-    if(date=='')
+   else if(date=='')
     {
         document.getElementById('dateid').style.cssText="border:1px solid red";
         document.getElementById('dateerror').innerHTML="select appropriate date";
-    }
-//    if(!isDateValid(date))
-//    {
-//     document.getElementById('dateid').style.cssText="border:1px solid red";
-//     document.getElementById('dateerror').innerHTML="select appropriate date";
-//    } 
-   if(Difference_In_Days >= 10){
+    } 
+   else if(Difference_In_Days >= 10){
     document.getElementById('dateid').style.cssText="border:1px solid red";
     document.getElementById('dateerror').innerHTML="Please Enter date upto 10 days ahead with rerspect the current date";
    }
-   if(Difference_In_Days < 0){
+   else if(Difference_In_Days < 0){
     document.getElementById('dateid').style.cssText="border:1px solid red";
     document.getElementById('dateerror').innerHTML="Please Enter a Valid Date for future trip";
    }
@@ -100,7 +101,7 @@ const postDataToServer = async (Url = '', data = {}) => {
             })
             .then(getDataFromServer('http://localhost:3000/getTravelInfo').then(function(getData)
             {
-                console.log("reached inside");
+               
                 updateUserInterface(getData);
             })
             );
@@ -125,6 +126,14 @@ const getDataFromServer=async (baseurl)=>
 //update data/UI on the use
 const updateUserInterface = (getData) => {
 
-
+    
+    console.log(getData.min_temp);
+    document.getElementById('min-temp').innerHTML=`Minimum Temp: ${getData[1].min_temp}`;
+    document.getElementById('max-temp').innerHTML=`Maximum Temp: ${getData[1].max_temp}`;
+    document.getElementById("mainimage").src = "`${getData[1].imageUrl}`";
+    console.log(`${getData[1].imageUrl}`);
+    document.getElementById("mainimage").style.display = "block";
+    document.getElementById("mainimage").setAttribute("src", `${getData[1].imageUrl}`);
+    
 }
 
